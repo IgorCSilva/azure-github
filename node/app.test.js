73f1ae8afcbfecf7d82a7c6c6d7bde91c90c101f
Igor Carneiro <igor.carneiro.silva13@gmail.com>
2022-04-26 18:23:41 -0300

    config

--- node/app.test.js
+++ node/app.test.js
@@ -1,8 +1,13 @@
 const calc = require('./app')
 
 test('test sum', sumTest)
+test('test sub', subTest)
 
 function sumTest() {
   expect(calc.sum(1, 2)).toBe(3 + 1)
   expect(calc.sum(2, 2)).toBe(4)
+}
+
+function sumTest() {
+  expect(calc.sub(2, 2)).toBe(1)
 }
