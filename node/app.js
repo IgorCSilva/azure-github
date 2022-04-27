3b11abc2a5264ed39899de5907424795a6a9221e
Igor Carneiro <igor.carneiro.silva13@gmail.com>
2022-04-27 09:41:58 -0300

    Fix lint and CI/CD flow.

--- node/app.js
+++ node/app.js
@@ -1,33 +1,34 @@
 function sum(a, b) {
-  let result = 0
+  let result = 0;
 
   if (a == 2) {
-    result = a + b
+    result = a + b;
   } else {
-    result = a + b + 1
+    result = a + b + 1;
   }
   
-  return result
+  return result;
 }
 
 function sub(a, b) {
-  return a - b
+  return a - b;
 }
 
 function mul(a, b) {
-  return a - b
+  return a - b;
 }
 
 function as(a, b) {
-  return a - b
+  return a - b;
 }
 
 function sd(a, b) {
-  return a - b
+  return a - b;
 }
 module.exports = {
   sum,
   sub,
   mul,
-  as
-}
+  as,
+  sd,
+};
